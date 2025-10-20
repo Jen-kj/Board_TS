afcbcce0fc3f35d03ba381684c9a4ce1166431b1
Jen-kj <jeon.jena.k@gmail.com>
2025-10-20 11:25:35 +0900

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: { browser: true, es2021: true, jest: true },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: 'detect' } },
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': ['warn', { endOfLine: 'auto' }]
+  }
+}
